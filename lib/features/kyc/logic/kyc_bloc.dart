46fd64c152c5acef665d4b098529ac64e194c24a
Kostiantyn Nechvolod <kostiantyn.nechvolod@gmail.com>
2021-09-27 16:27:46 +0300

    Added KycEvent

--- lib/features/kyc/logic/kyc_bloc.dart
+++ lib/features/kyc/logic/kyc_bloc.dart
@@ -2,12 +2,12 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_template/utils/view/models/name.dart';
 import 'package:flutter_template/utils/view/models/string_field.dart';
 import 'package:formz/formz.dart';
 
 part 'kyc_event.dart';
-
 part 'kyc_state.dart';
 
 class KycBloc extends Bloc<KycEvent, KycState> {
--- lib/features/kyc/logic/kyc_event.dart
+++ lib/features/kyc/logic/kyc_event.dart
@@ -2,4 +2,159 @@ part of 'kyc_bloc.dart';
 
 abstract class KycEvent extends Equatable {
   const KycEvent();
+
+  @override
+  List<Object?> get props => [];
 }
+
+class ProfileImageChanged extends KycEvent {
+  const ProfileImageChanged({@required this.image});
+
+  final String? image;
+
+  @override
+  List<Object?> get props => [image];
+}
+
+class FirstNameChanged extends KycEvent {
+  const FirstNameChanged({required this.firstName});
+
+  final String? firstName;
+
+  @override
+  List<Object?> get props => [firstName];
+}
+
+class LastNameChanged extends KycEvent {
+  const LastNameChanged({
+    @required this.lastName,
+  });
+
+  final String? lastName;
+
+  @override
+  List<Object?> get props => [lastName];
+}
+
+class NationalityChanged extends KycEvent {
+  const NationalityChanged({
+    @required this.nationality,
+  });
+
+  final String? nationality;
+
+  @override
+  List<Object?> get props => [nationality];
+}
+
+class PhoneNumberChanged extends KycEvent {
+  const PhoneNumberChanged({
+    @required this.phoneNumber,
+  });
+
+  final String? phoneNumber;
+
+  @override
+  List<Object?> get props => [phoneNumber];
+}
+
+class NationalInsuranceNumberChanged extends KycEvent {
+  const NationalInsuranceNumberChanged({
+    @required this.nationalInsuranceNumber,
+  });
+
+  final String? nationalInsuranceNumber;
+
+  @override
+  List<Object?> get props => [nationalInsuranceNumber];
+}
+
+class TaxIdChanged extends KycEvent {
+  const TaxIdChanged({
+    @required this.taxId,
+  });
+
+  final String? taxId;
+
+  @override
+  List<Object?> get props => [taxId];
+}
+
+class IdentityCardNumberChanged extends KycEvent {
+  const IdentityCardNumberChanged({
+    @required this.identityCardNumber,
+  });
+
+  final String? identityCardNumber;
+
+  @override
+  List<Object?> get props => [identityCardNumber];
+}
+
+class HighestSchoolNumberChanged extends KycEvent {
+  const HighestSchoolNumberChanged({
+    @required this.highestSchoolNumber,
+  });
+
+  final String? highestSchoolNumber;
+
+  @override
+  List<Object?> get props => [highestSchoolNumber];
+}
+
+class VocationTrainingCertificateChanged extends KycEvent {
+  const VocationTrainingCertificateChanged({
+    @required this.vocationTrainingCertificate,
+  });
+
+  final String? vocationTrainingCertificate;
+
+  @override
+  List<Object?> get props => [vocationTrainingCertificate];
+}
+
+class AddressChanged extends KycEvent {
+  const AddressChanged({
+    @required this.address,
+  });
+
+  final String? address;
+
+  @override
+  List<Object?> get props => [address];
+}
+
+class SexChanged extends KycEvent {
+  const SexChanged({
+    @required this.sex,
+  });
+
+  final String? sex;
+
+  @override
+  List<Object?> get props => [sex];
+}
+
+class DateOfBirthChanged extends KycEvent {
+  const DateOfBirthChanged({
+    @required this.dateOfBirth,
+  });
+
+  final String? dateOfBirth;
+
+  @override
+  List<Object?> get props => [dateOfBirth];
+}
+
+class IbanChanged extends KycEvent {
+  const IbanChanged({
+    @required this.iban,
+  });
+
+  final String? iban;
+
+  @override
+  List<Object?> get props => [iban];
+}
+
+class FormSubmitted extends KycEvent {}
